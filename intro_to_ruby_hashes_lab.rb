32ccf314c685bb92268115a09110963eed7059ac
Brad Alexander <brad.alexander913@gmail.com>
2019-08-05 00:53:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
    solohash = { 
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {:keyone, :keytwo, :keythree, :keyfour}
     }  
   }
 end
